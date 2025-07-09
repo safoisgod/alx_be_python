4e8c210d45cf99b064a951bcd48a2427da764b96
Nana Safo <iamnanasafo@gmail.com>
2025-07-09 15:08:10 +0000

    aded basic_operations.py with 3 arithmetic operations practice

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print(f"sum of {number1} and {number2} is {number1+number2}")
+print(f"difference of {number1} and {number2} is {number1-number2}")
+print(f"product of {number1} and {number2} is {number1*number2}")
